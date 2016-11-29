114e81b6a9c72f73eb895de92791ab6490ff8825
jotonyguerra <jotonyguerra@gmail.com>
2016-11-28 18:07:58 -0800

    Create thrifty.rb

---
+++ thrifty.rb
@@ -0,0 +1,81 @@
+##Our first box has a number of mixed items in it. Let's use Ruby to see if the box contains any denim shirts for Dylan to try on. The output should be true if the box contains denim shirts and false otherwise.
+box_1 =
+["flannel shirt", "bucket hat", "pipe","converse shoes",
+"purple hair dye", "Ray Ban sunglasses", "combat boots",
+"Warby Parker glasses", "overalls", "Doc Marten shoes",
+"suspenders", "Fjallraven backpack", "wrap belt",
+"denim cut-offs", "beanie", "muscle tank", "Hawaiian shirt",
+"scarf", "misplaced handlebar mustache", "high-waisted jeans",
+"denim shirt", "statement necklace", "knitted cardigan vest",
+"stockings", "black leggings", "polka dot blouse", "kimono",
+"cut-off shorts", "grandma's cardigan", "high-rise shorts",
+"tartan shirt", "converse shoes", "fedora hat",
+"brown flat shoes", "red nail polish", "polka dot tights",
+"bracelet", "leather jacket", "skinny jeans",
+"silver nail polish", "matte lipstick", "v-neck jumper",
+"scarf", "beige shearling jacket", "fossil flower collar necklace",
+"stone cross necklace", "loose knit tank top",
+"mock neck open back tank top", "sequin tank top", "red sweater"]
+if box_1.include?("denim shirt")
+  puts "The box includes a denim shirt"
+else
+  puts "This box does not include a denim shirt"
+end
+if box_1.any? {|polish| polish == "black nail polish"}
+  puts "the box contains black nail polish"
+elsif box_1.any? {|polish| polish == "silver nail polish"}
+  puts "Unfortunately, this box does not contain black nail polish.
+The box contains silver nail polish."
+else
+  puts "this box does not contain black or silver nail polish"
+end
+
+box_2 =
+["Doc Martens Women's 1460 Re-Invented Victorian Print Lace Up Boot",
+"Red Patent Lather Cutie Pie Pumps",
+"Gold Metallic & Rhinestone Peep Toe Flats",
+"White Bow Mila Flats",
+"Royal Blue Classic Lace Up Sneakers Flats",
+"White Patent Leather Button Up T-Strap Heels",
+"Black Suede Faux Fur Open Toe Heels",
+"Yellow Hello Sunshine Sonia Loafers",
+"TUK Black Vintage Floral Vegan Starlet Pumps",
+"White T-Strap Mary Jane Kitten Heels",
+"Clear & Silver Glitter Heart Heels",
+"Light Green & White Feather Pom Pom Mila Flats",
+"White Classic Lace Up Sneaker Flats",
+"Irregular Choice Glitter Star Wars The Golden Droid C-3PO Flats",
+"Mint & White Color Block Emmie Saddle Shoes"]
+## used each_with_index to format index into a more legible list
+
+
+box_2.each_with_index do |shoe, index|
+  puts "#{index + 1}. #{shoe}\n"
+end
+
+# third box
+box_3 =
+["skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans",
+"skinny jeans", "skinny jeans", "skinny jeans"]
+if box_3.all? {|jeans| jeans=="skinny jeans" }
+  puts "The box is all skinny jeans"
+else
+  puts "The box is not all skinny jeans"
+end
+## didnt use .map didnt feel the need to edit the arrays given the assignment

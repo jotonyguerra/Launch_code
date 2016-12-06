e942467aec6d3ac4d3834e953f12441bfdd01e78
jotonyguerra <jotonyguerra@gmail.com>
2016-12-05 22:26:35 -0800

    Create right_clubs.rb

---
+++ right_clubs.rb
@@ -0,0 +1,33 @@
+REQUIRED_BAG_SIZE = 10
+
+available_clubs = [
+  :two_iron,
+  :three_iron,
+  :four_iron,
+  :five_iron,
+  :six_iron,
+  :seven_iron,
+  :eight_iron,
+  :nine_iron,
+  :pitching_wedge,
+  :sand_wedge,
+  :driver,
+  :three_wood,
+  :five_wood,
+  :seven_wood,
+  :putter
+]
+
+possible_club_selections = available_clubs.sample(REQUIRED_BAG_SIZE)
+
+# YOUR CODE GOES BELOW
+puts "*** Automated Golf Club Advice:***"
+possible_club_selections.each do |club|
+  puts "Use the #{club}!"
+end
+REQUIRED_CLUBS = [:driver, :pitching_wedge, :putter]
+## why gsub "" with " "??? why not just put required_club.to_s??
+missing_required_clubs = REQUIRED_CLUBS - possible_club_selections
+missing_required_clubs.each do |required_club|
+  puts "WARNING! You will be without a #{required_club.to_s}"
+end

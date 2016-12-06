9e5fc4b00e0c6630ab6afce80ebf2ef74e6e4849
jotonyguerra <jotonyguerra@gmail.com>
2016-12-06 12:37:12 -0800

    Create acronyms.rb

---
+++ acronyms.rb
@@ -0,0 +1,13 @@
+acronyms = {
+  "EST" => "Eastern Standard Time",
+  "EOF" => "End of File",
+  "SSN" => "Social Security Number",
+  "UFO" => "Unidentified Flying Object",
+  "NASA" => "National Aeronautics and Space Administration",
+  "MTV" => "Music Television",
+  "DAEMON" => "Disk And Execution Monitor"
+}
+
+acronyms.each do |key, value|
+  puts "#{key}: #{value}"
+end
